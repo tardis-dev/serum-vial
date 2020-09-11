2d55e9edc4785ad19ab1962f74a44425dad4a986
Thaddeus <thad@tardis.dev>
2020-09-11 14:32:29 +0200

    0.1.1

--- test/serummachine.test.ts
+++ test/serummachine.test.ts
@@ -1,19 +1,3 @@
-import { SerumMachine } from '../src'
-
-const PORT = 8072
-// const WS_STREAMS_URL = `ws://localhost:${PORT}/stream`
-
 describe('Serum Machine', () => {
-  let serumMachine: SerumMachine
-
-  beforeAll(async () => {
-    serumMachine = new SerumMachine({ nodeEndpoint: 'https://solana-api.projectserum.com' })
-    await serumMachine.start(PORT) // start server
-  })
-
-  afterAll(async () => {
-    await serumMachine.stop()
-  })
-
   test('hello', () => {})
 })
